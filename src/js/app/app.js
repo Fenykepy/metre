fbd585c706482e6f5caf00e25b47c1ca0e87bdf0
Fenykepy <phiroom@lavilotte-rolle.fr>
2015-07-31 21:58:23 +0200

    bootstrap app

---
+++ src/js/app/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var React = require('react');
+
+
+var Metre  = require('./components/Metre');
+
+
+
+// render main component
+React.render(
+    <Metre/>,
+    document.body
+);
+
+
